adc2e704ead0a47f5761f9d0de811d60ccd21e82
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-05-01 17:56:44 +0700

    feat: add cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/sokungz01/cpe241-project-backend/config"
 	"github.com/sokungz01/cpe241-project-backend/platform"
 	"github.com/sokungz01/cpe241-project-backend/routes"
@@ -22,6 +23,10 @@ func main() {
 	}
 
 	api := fiber.New()
+	api.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+	}))
 	routes.RoutesRegister(api, myDB, cfg)
 	api.Listen(":3000")
 }
