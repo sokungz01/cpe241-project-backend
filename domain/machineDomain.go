584d4e5a7dec955ac6675734f1ffda7342117fdf
Sorn.Udom <sorrawit.udom@mail.kmutt.ac.th>
2024-05-07 00:05:58 +0700

    machineDomain.go Change

--- domain/machineDomain.go
+++ domain/machineDomain.go
@@ -14,26 +14,3 @@ type Machine struct {
 	Description   string    `json:"desc" db:"desciption"`
 	Status        bool      `json:"status" db:"status"`
 }
-
-type MachineType struct {
-	MachineTypeID   int    `json:"machinetypeid" db:"machineTypeID"`
-	MachineTypeName string `json:"machinetypename" db:"machineTypeName"`
-}
-
-type MachineRepository interface {
-	GetAll() (*[]Machine, error)
-
-	CreateMachineType(mtype MachineType) error
-	GetOneMachineTypeByName(typeName string) (*MachineType, error)
-	GetOneMachineTypeByID(id int) (*MachineType, error)
-	UpDateMachineType(id int, newData MachineType) (*MachineType, error)
-	DeleteMachineType(id int) error
-}
-
-type MachineUseCase interface {
-	GetAll() (*[]Machine, error)
-	CreateMachineType(mtype MachineType) error
-	GetOneMachineTypeByName(typeName string) (*MachineType, error)
-	UpDateMachineType(id int, newData MachineType) (*MachineType, error)
-	DeleteMachineType(id int) error
-}
