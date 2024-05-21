7836277ae180e8c5a8dd1ef3e59a84a9b87c3cb1
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-05-21 11:30:18 +0700

    feat(itemlog) | join table

--- domain/itemLog.domain.go
+++ domain/itemLog.domain.go
@@ -1,11 +1,15 @@
 package domain
 
+import "time"
+
 type ItemLog struct {
-	ItemLogID  int `json:"itemLogID" db:"itemLogID"`
-	ItemID     int `json:"itemID" db:"itemID"`
-	ItemQty    int `json:"qty" db:"qty"`
-	StaffID    int `json:"staffID" db:"staffID"`
-	CreateDate int `json:"createDate" db:"createDate"`
+	ItemLogID  int       `json:"itemLogID" db:"itemLogID"`
+	ItemID     int       `json:"itemID" db:"itemID"`
+	ItemQty    int       `json:"qty" db:"qty"`
+	StaffID    int       `json:"staffID" db:"staffID"`
+	CreateDate time.Time `json:"createDate" db:"createdDate"`
+	Staff      User      `json:"staff" db:",prefix=employee."`
+	Item       Item      `json:"item" db:",prefix=inventory."`
 }
 
 type ItemLogUsecase interface {
--- repository/itemLog.usecase.go
+++ repository/itemLog.usecase.go
@@ -15,8 +15,11 @@ func NewItemLogRepository(db *platform.Mysql) domain.ItemLogRepository {
 
 func (r *ItemLog) GetAll() (*[]domain.ItemLog, error) {
 	response := make([]domain.ItemLog, 0)
-	if err := r.db.Select(&response, "SELECT * FROM `itemLog`"); err != nil {
+	if err := r.db.Select(&response, "SELECT * FROM `itemLog` AS il INNER JOIN `inventory` AS item ON item.itemID = il.itemID INNER JOIN `employee` ON employee.employeeID = il.staffID"); err != nil {
 		return nil, err
+	}
+	for i := range response {
+		response[i].Staff.Password = ""
 	}
 	return &response, nil
 }
